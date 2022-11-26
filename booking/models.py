e70e708e100acfc7c0d0e2d373006e0c5e7c6d0c
Erik Vodopivec Forsman <108095837+11bus11@users.noreply.github.com>
2022-11-26 18:07:06 +0100

    Added datamodel
    Added the datamodel for the bookings

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+STATUS = ((0, "available"), (1, "booked"))
+PLAYS = ((0, "a christmas carol"), (1, "phantom of the opera"))
+
+class Booking(models.Model):
+    play = models.IntegerField(choices=PLAYS)
+    date = models.DateTimeField()
+    time = models.DateTimeField()
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.name
