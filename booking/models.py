b7f9a6803cbe69df61deed00aad4006537410012
Erik Vodopivec Forsman <02linfor01@gmail.com>
2022-11-27 23:53:09 +0000

    Added model for plays and made a basic base.html

--- booking/models.py
+++ booking/models.py
@@ -5,33 +5,30 @@ from django.contrib.auth.models import User
 
 STATUS = ((0, "available"), (1, "booked"))
 PLAYS = ((0, "a christmas carol"), (1, "phantom of the opera"))
-
-class Booking(models.Model):
-    play = models.IntegerField(choices=PLAYS)
-    date = models.DateField()
-    time = models.TimeField()
-    name = models.CharField(max_length=200)
-    email = models.EmailField()
-    status = models.IntegerField(choices=STATUS, default=0)
-
-    class Meta:
-        ordering = ['-date']
-
-    def __str__(self):
-        return self.name
+DATE = ((0, "10/12"), (1, "13/12"), (2, "17/12"), (3, "20/12"), (4, "23/12"))
+TIME = ((0, "12:30"), (1, "16:00"))
 
 
-class User(models.Model):
-    email = models.EmailField()
-    password = models.CharField(max_length=80)
 
-    class Meta:
-        ordering = ['email']
-
-class Now_playing(models.Model):
+class NowPlaying(models.Model):
     play = models.IntegerField(choices=PLAYS)
     status = models.IntegerField(choices=STATUS, default=0)
     seats = models.IntegerField(41)
+    date = models.IntegerField(choices=DATE)
+    time = models.IntegerField(choices=TIME)
 
     class Meta:
         ordering = ['status']
+
+class Booking(models.Model):
+    play = models.IntegerField(choices=PLAYS)
+    time = models.ForeignKey(NowPlaying, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    email = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ['-time']
+
+    def __str__(self):
+        return self.name
--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Booking
+from .models import NowPlaying
+
+class PlayList(generic.ListView):
+    model = NowPlaying
+    queryset = 
+    template_name = "plays.html"
+
+class Booking(generic.ListView):
+    model = Booking
+    queryset = 
+    template_name = "user.html"
 
