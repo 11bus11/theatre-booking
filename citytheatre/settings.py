03c3104f4180bf1b00bcdaae60e10c71f9c6bd9d
Erik Vodopivec Forsman <02linfor01@gmail.com>
2022-12-20 11:42:28 +0000

    Tried to solve server running problem. Not solved

--- citytheatre/settings.py
+++ citytheatre/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = 'citytheatre.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
