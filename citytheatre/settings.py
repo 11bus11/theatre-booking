f86add027ab19cda062071cd18aa12148303a0b3
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-26 20:22:59 +0000

    setting changes

--- citytheatre/settings.py
+++ citytheatre/settings.py
@@ -48,6 +48,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     # The following apps are required:
     'django.contrib.auth',
     'django.contrib.messages',
@@ -91,7 +92,7 @@ ACCOUNT_EMAIL_REQUIRED = True
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
-LOGIN_REDIRECT_URL = '/success'
+LOGIN_REDIRECT_URL = '/'
 
 WSGI_APPLICATION = 'citytheatre.wsgi.application'
 
