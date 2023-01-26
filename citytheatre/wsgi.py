e4d835e77f1e562d4aba229b64721988b4019e82
Erik Vodopivec Forsman <02linfor01@gmail.com>
2023-01-26 20:53:46 +0000

    wsgi

--- citytheatre/wsgi.py
+++ citytheatre/wsgi.py
@@ -12,12 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
 
-from my_project import MyWSGIApp
-
-application = MyWSGIApp()
-application = WhiteNoise(application, root="/path/to/static/files")
-application.add_files("/path/to/more/static/files", prefix="more-files/")
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'citytheatre.settings')
 
 application = get_wsgi_application()
